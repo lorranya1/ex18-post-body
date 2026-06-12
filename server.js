fdaa5fe3a42e5341d98e4a7f47bb79b2370534dd
lorranya1 <lar2@aluno.ifal.edu.br>
2026-06-12 15:45:14 -0300

    Update server.js

--- server.js
+++ server.js
@@ -3,6 +3,10 @@ const app = express();
 
 // TODO: Adicione o middleware express.json() para parsear o body
 // POST /echo → 200, devolve o mesmo JSON recebido no body
+app.use(express.json());
 // O Content-Type da resposta deve ser application/json
+app.post('/echo', (req, res) => {
+  res.json(req.body);
+});
 
 app.listen(3000, () => console.log('Servidor rodando em http://localhost:3000'));
